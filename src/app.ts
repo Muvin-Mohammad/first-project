2ba66aa03e72c56719baa8d5416b4632aebab110
Muvin <muvin.mohammad2@gmail.com>
2024-12-02 15:44:12 +0600

    done

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
-import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/config/modules/student/student.route';
 
 const app: Application = express();
 
-// parser
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req: Request, res: Response) => {
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
   const a = 10;
-  res.json({ value: a }); // Correct way to send a JSON response
-});
+  res.send(a);
+};
+
+app.get('/', getAController);
 
 export default app;
